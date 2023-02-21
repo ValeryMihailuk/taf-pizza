9054d02ab56941b1bbc2cb6e79fb3b517ce0217b
ValeryMihailuk <valery.mihkailuk@gmail.com>
2023-02-21 15:42:35 +0300

    refactor terrapizzapage, functional and testcartwithmargarita class

--- src/main/java/by/itacademy/valerymihailuk/web/TerrapizzaPage.java
+++ src/main/java/by/itacademy/valerymihailuk/web/TerrapizzaPage.java
@@ -2,7 +2,7 @@ package by.itacademy.valerymihailuk.web;
 
 public class TerrapizzaPage {
     public static final String URL = "https://terrapizza.by/";
-    public static final String PAGE_PIZZA = "/html/body/div[3]/div[2]/div/div/div/div/div/ul/li[10]/a";
+    public static final String PAGE_PIZZA = "/html/body/div[3]/div[2]/div/div/div/div/div/ul/li[10]";
     public static final String MARGARITA_IN_PIZZA = "//*[@id=\"menu-card-364\"]/div[3]/div[3]/div[2]/div[2]/button";
     public static final String CARD = "/html/body/div[2]/div[1]/div[3]/div[2]/div[1]";
     public static final String MARGARITA_IN_CARD = "//*[@id=\"basket-el-0\"]/div[2]/div[1]/div[1]";
--- src/test/java/by/itacademy/valerymihailuk/web/TestCartWithMargarita.java
+++ src/test/java/by/itacademy/valerymihailuk/web/TestCartWithMargarita.java
@@ -1,6 +1,5 @@
 package by.itacademy.valerymihailuk.web;
 
-
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,29 +7,29 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class TestCartWithMargarita {
     WebDriver driver;
 
     @BeforeEach
     public void openTerrapizza() {
-
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get(TerrapizzaPage.URL);
     }
 
     @Test
     public void testCard() {
         WebElement catalogPizza = driver.findElement(By.xpath(TerrapizzaPage.PAGE_PIZZA));
-        catalogPizza.click();
+        catalogPizza.();
         WebElement margarita = driver.findElement(By.xpath(TerrapizzaPage.MARGARITA_IN_PIZZA));
         margarita.click();
         WebElement card = driver.findElement(By.xpath(TerrapizzaPage.CARD));
         card.click();
         WebElement cardWithMargarita = driver.findElement(By.xpath(TerrapizzaPage.MARGARITA_IN_CARD));
-        Assertions.assertEquals("Пицца Маргарита", cardWithMargarita);
+        Assertions.assertEquals("Пицца Маргарита", cardWithMargarita.getText());
     }
 
     @AfterEach
