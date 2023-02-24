d1b2e233ca60d6cfb89050d731b193ca50f00476
ValeryMihailuk <valery.mihkailuk@gmail.com>
2023-02-24 14:10:27 +0300

    refactor testcartwithmargarita class

--- src/main/java/by/itacademy/valerymihailuk/web/TerrapizzaPage.java
+++ src/main/java/by/itacademy/valerymihailuk/web/TerrapizzaPage.java
@@ -3,13 +3,14 @@ package by.itacademy.valerymihailuk.web;
 public class TerrapizzaPage {
     public static final String URL = "https://terrapizza.by/";
     public static final String COOKIES_CLICK="/html/body/div[6]/div/button";
-    public static final String PAGE_PIZZA = "/html/body/div[3]/div[2]/div/div/div/div/div/ul/li[10]";
-    public static final String MARGARITA_IN_PIZZA = "//*[@id=\"menu-card-364\"]/div[3]/div[3]/div[2]/div[2]/button";
-    public static final String CARD = "/html/body/div[2]/div[1]/div[3]/div[2]/div[1]/div[1]";
+    public static final String PAGE_PIZZA = "//li[10]/a [@href =\"/catalog/picca\"]";
+    public static final String MARGARITA_IN_PIZZA = "//div/button [@data-id=\"364\"]";
+    public static final String CARD = "//div [@class=\"basket__btn-top basket__top basket__btn-top--sm\"]";
 
-    public static final String MARGARITA_IN_CARD = "/html/body/div[5]/div[2]/div[2]/ul/li[1]/div[2]/div[1]/div[1]";
+    public static final String MARGARITA_IN_CARD = "//div [@class=\"basket__products-item-name\"]";
 
-    public static final String PAGE_BAR = "/html/body/div[3]/div[1]/div/div/div[1]/div/div/div/ul/li[16]/a";
-    public static final String COLA_IN_BAR = "//*[@id=\"menu-card-1736\"]/div[3]/div[2]/div/div[2]/button";
-    public static final String COLA_IN_CARD = "/html/body/div[5]/div[2]/div[2]/ul/li[2]/div[2]/div[1]/div[1]";
+    public static final String PAGE_BAR = "//li[16]/a [@href=\"/catalog/bar\"]";
+    public static final String COLA_IN_BAR = "//div/button [@data-id=\"1736\"]";
+    public static final String COLA_IN_CARD = "//li[2]/div[2]/div/div [@class=\"basket__products-item-name\"]";
 }
+
